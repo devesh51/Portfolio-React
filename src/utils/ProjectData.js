95ad8808a101d31cdfe011e888b78faecd1723fd
Devesh upadhyay <deveshupadhyay49@gmail.com>
2024-06-22 11:50:45 +0530

    added links in projects

--- src/utils/ProjectData.js
+++ src/utils/ProjectData.js
@@ -1,21 +1,31 @@
+import videoMovix from "../utils/Videos/Movix-project-clip.mp4";
+// import deveshPortfolio from "../utils/Videos/Devesh-portfolio.mp4";
+import PMS from "../utils/Videos/PMS_video.mp4";
+import youtubeClone from "../utils/Videos/youtube-clone.mp4";
 export const ProjectsData = [
   {
     id: 1,
-    projectName: "Ecommerce web app",
+    projectName: "Movix",
+    url: "https://movies-project-458a0.web.app/",
+    video: videoMovix,
     ProjectHeading: "Tranfser money to anyone, instantly! No fees, no BS.",
     ProjectDetails:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
     id: 2,
-    projectName: "Weather web app",
+    projectName: "Project Management System",
+    video: PMS,
+    url: "http://65.1.109.92/",
     ProjectHeading: "Tranfser money to anyone, instantly! No fees, no BS.",
     ProjectDetails:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
     id: 3,
-    projectName: "Portfolio web app",
+    projectName: "Youtube Clone",
+    url: "https://www.youtube.com/",
+    video: youtubeClone,
     ProjectHeading: "Tranfser money to anyone, instantly! No fees, no BS.",
     ProjectDetails:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
